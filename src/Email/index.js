0c377005e0e8b89bf85de4ada1459213cc5c3d8f
ize8 <35601187+ize8@users.noreply.github.com>
2020-06-24 23:04:11 +0800

    rebase

--- src/Email/index.js
+++ src/Email/index.js
@@ -10,7 +10,7 @@ exports.sendTestEmail = () => {
     from: "Excited User <me@mg.wordplay.donotpanic.cc>",
     to: "zavori.laszlo@gmail.com",
     subject: "It's working!",
-    text: "Testing some Mailgun awesomness"
+    text: "Testing some Mailgun awesomness!"
   };
   return new Promise((resolve, reject) => {
     mg.messages().send(data, function(error, body) {
