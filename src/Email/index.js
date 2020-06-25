ccbc36037c8b14a8c325ac0fc8d047ff68693636
ize8 <35601187+ize8@users.noreply.github.com>
2020-06-26 00:59:43 +0800

    MAilgun finally works with template

--- src/Email/index.js
+++ src/Email/index.js
@@ -1,14 +1,21 @@
 const mailgun = require("mailgun-js")({
   apiKey: process.env.MAILGUN_API_KEY,
-  domain: "mg.wordplay.donotpanic.cc"
+  domain: process.env.MAILGUN_DOMAIN,
+  host: "api.eu.mailgun.net"
 });
 
 exports.sendTestEmail = () => {
   const data = {
-    from: "Excited User <me@mg.wordplay.donotpanic.cc>",
+    from: "WordPlay App <donot-reply@mg.wordplay.donotpanic.cc>",
     to: "zavori.laszlo@gmail.com",
     subject: "It's working",
-    text: "Testing some Mailgun awesomness!"
+    // text: "Testing some Mailgun awesomness!",
+    // html: "<h1>It works!</h1>"
+    template: "validate-email",
+    "h:X-Mailgun-Variables": JSON.stringify({
+      name: "Csirkekoma",
+      link: "https://word-play-1.herokuapp.com/"
+    })
   };
   return new Promise((resolve, reject) => {
     mailgun.messages().send(data, function(error, body) {
